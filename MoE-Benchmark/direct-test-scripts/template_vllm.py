deb5eb8ab35d59e59dcd81c1aabec91a621d48d2
jpr-teas <jpr-teas@eidf230-dev1.vms.os.eidf.epcc.ed.ac.uk>
2026-04-05 18:51:48 +0000

    update template replacement logic to avoid python and bash {} clash

--- MoE-Benchmark/direct-test-scripts/template_vllm.py
+++ MoE-Benchmark/direct-test-scripts/template_vllm.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-from datetime import datetime
+import os
 from utils import get_run_name
 
 class Template:
@@ -18,14 +18,30 @@ class Template:
         num_gpu: int, \
         gpu_product: str):
 
-        timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+        #timestamp = datetime.now().strftime("%Y%m%d-%H%M")
         model_name_clean=model_name.split("/")[1].replace(".", "-")
         gpu=gpu_product.split("-")[1] 
 
         run_name = get_run_name("vllm", model_name, gpu_product, num_gpu, target_input_tokens, target_output_tokens, batch_size, dataset)
         run_name_lower = run_name.replace("_", "-").lower()
-        
-        return f"""
+       
+        with open(os.path.expanduser("yaml_templates/template_vllm.yaml"), "r") as f:
+          template = f.read()
+
+        template = template.replace("@model_name@", str(model_name))
+        template = template.replace("@tensor_parallel_size@", str(tensor_parallel_size))
+        template = template.replace("@dataset@", str(dataset))
+        template = template.replace("@num_gpu@", str(num_gpu))
+        template = template.replace("@target_input_tokens@", str(target_input_tokens))
+        template = template.replace("@target_output_tokens@", str(target_output_tokens))
+        template = template.replace("@num_samples@", str(num_samples))
+        template = template.replace("@gpu_product@", str(gpu_product))
+        template = template.replace("@batch_size@", str(batch_size))
+        template = template.replace("@run_name@", str(run_name))
+
+        return template
+
+"""
 apiVersion: batch/v1
 kind: Job
 metadata:
